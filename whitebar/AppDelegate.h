999b87b63dd26303c4a7d897bd24de56e36576e3
George Nachman <gnachman@gmail.com>
2020-11-05 23:39:38 -0800

    Initial Commit

---
+++ whitebar/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  whitebar
+//
+//  Created by George Nachman on 11/5/20.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
